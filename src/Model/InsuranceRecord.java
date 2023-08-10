0e7e335c4726b9fce66e239d39a469d6e1eedac7
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-10 23:06:54 +0600

    Update InsuranceRecord.java

--- src/Model/InsuranceRecord.java
+++ src/Model/InsuranceRecord.java
@@ -4,10 +4,55 @@
  */
 package Model;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
 /**
  *
  * @author Asus
  */
-public class InsuranceRecord {
+public class InsuranceRecord implements Serializable{
+    private static final long serialVersionUID = 13L;
+    
+    private String item;
+    private Double insuranceAmount;
+    private LocalDate dateOfIssue;
+
+    public InsuranceRecord(String item, Double insuranceAmount, LocalDate dateOfIssue) {
+        this.item = item;
+        this.insuranceAmount = insuranceAmount;
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Double getInsuranceAmount() {
+        return insuranceAmount;
+    }
+
+    public void setInsuranceAmount(Double insuranceAmount) {
+        this.insuranceAmount = insuranceAmount;
+    }
+
+    public LocalDate getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(LocalDate dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    @Override
+    public String toString() {
+        return "InsuranceRecord{" + "item=" + item + ", insuranceAmount=" + insuranceAmount + ", dateOfIssue=" + dateOfIssue + '}';
+    }
+    
+    
     
 }
