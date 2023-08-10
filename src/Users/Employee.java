b3aeacf73bce127048dc1dda0a3941ab9fabf201
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-10 22:02:59 +0600

    saved all the files

--- src/Users/Employee.java
+++ src/Users/Employee.java
@@ -6,18 +6,72 @@ package Users;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 /**
  *
  * @author Asus
  */
-public abstract class Employee extends User implements Serializable{
+public abstract class Employee extends User implements Serializable {
+
     private static final long serialVersionUID = 13L;
-    
+
     public String designation;
     protected double salary;
     protected final LocalDate DOJ;
+
+    public Employee(String name, Integer ID, String password, String email, String gender, LocalDate DOB, String Designation, Double Salary,
+        LocalDate DoJ) {
+        super(name, ID, password, email, gender, DOB);
+        this.designation = designation;
+        this.salary = salary;
+        this.DOJ = null;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "designation=" + designation + ", salary=" + salary + ", DOJ=" + DOJ + '}';
+    }
     
+
     
 }
