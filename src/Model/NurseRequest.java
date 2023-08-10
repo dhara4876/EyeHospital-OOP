ae86d1c3165a537e931e9b7f7e0c855707f1dbfb
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-10 23:14:51 +0600

    Update NurseRequest.java

--- src/Model/NurseRequest.java
+++ src/Model/NurseRequest.java
@@ -4,10 +4,52 @@
  */
 package Model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Asus
  */
-public class NurseRequest {
+public class NurseRequest implements Serializable {
+    private static final long serialVersionUID = 13L;
+    
+    private Integer nurseId;
+    private Integer doctorId;
+    private String requestDetails;
+
+    public NurseRequest(Integer nurseId, Integer doctorId, String requestDetails) {
+        this.nurseId = nurseId;
+        this.doctorId = doctorId;
+        this.requestDetails = requestDetails;
+    }
+
+    public Integer getNurseId() {
+        return nurseId;
+    }
+
+    public void setNurseId(Integer nurseId) {
+        this.nurseId = nurseId;
+    }
+
+    public Integer getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Integer doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getRequestDetails() {
+        return requestDetails;
+    }
+
+    public void setRequestDetails(String requestDetails) {
+        this.requestDetails = requestDetails;
+    }
+    
+    
+    
+    
+    
     
 }
