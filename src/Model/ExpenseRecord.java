fe9d57b3da1ba7a1a5f8f7ed2fe45030cb8f2448
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-10 22:54:22 +0600

    Update ExpenseRecord.java

--- src/Model/ExpenseRecord.java
+++ src/Model/ExpenseRecord.java
@@ -4,10 +4,55 @@
  */
 package Model;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
 /**
  *
  * @author Asus
  */
-public class ExpenseRecord {
+public class ExpenseRecord implements Serializable {
+    private static final long serialVersionUID = 13L; 
+    private Double Amount;
+    private String SpentOn;
+    private LocalDate DateSpent;
+
+    public ExpenseRecord(Double Amount, String SpentOn, LocalDate DateSpent) {
+        this.Amount = Amount;
+        this.SpentOn = SpentOn;
+        this.DateSpent = DateSpent;
+    }
+
+    public Double getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(Double Amount) {
+        this.Amount = Amount;
+    }
+
+    public String getSpentOn() {
+        return SpentOn;
+    }
+
+    public void setSpentOn(String SpentOn) {
+        this.SpentOn = SpentOn;
+    }
+
+    public LocalDate getDateSpent() {
+        return DateSpent;
+    }
+
+    public void setDateSpent(LocalDate DateSpent) {
+        this.DateSpent = DateSpent;
+    }
+
+    @Override
+    public String toString() {
+        return "ExpenseRecord{" + "Amount=" + Amount + ", SpentOn=" + SpentOn + ", DateSpent=" + DateSpent + '}';
+    }
     
+    
+    
+     
 }
