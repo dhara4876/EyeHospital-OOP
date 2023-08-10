93214be75980960f28c1d344d3a773e5e411ba88
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-10 23:10:44 +0600

    Update Medicine.java

--- src/Model/Medicine.java
+++ src/Model/Medicine.java
@@ -4,10 +4,45 @@
  */
 package Model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Asus
  */
-public class Medicine {
+public class Medicine implements Serializable{
+    private static final long serialVersionUID = 13L;
+    
+    private String medicineName;
+    private Double medicinePrice;
+
+    public Medicine(String medicineName, Double medicinePrice) {
+        this.medicineName = medicineName;
+        this.medicinePrice = medicinePrice;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public Double getMedicinePrice() {
+        return medicinePrice;
+    }
+
+    public void setMedicinePrice(Double medicinePrice) {
+        this.medicinePrice = medicinePrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Medicine{" + "medicineName=" + medicineName + ", medicinePrice=" + medicinePrice + '}';
+    }
+    
+    
+    
     
 }
