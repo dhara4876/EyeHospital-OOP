3b3bf7b7e88edd5cbc95513fd864269da5c3fc1a
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-10 23:02:28 +0600

    made medicine class and edited bill class

--- src/Model/Bill.java
+++ src/Model/Bill.java
@@ -4,10 +4,86 @@
  */
 package Model;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
 /**
  *
  * @author Asus
  */
-public class Bill {
+public class Bill implements Serializable {
+    private static final long serialVersionUID = 13L;
+    private Integer patientId;
+    private Double totalDue;
+    private LocalDate billedOn;
+    private Localdate dueBy;
+    private Boolean paidStatus;
+    private Integer accountantId;
+
+    public Bill(Integer patientId, Double totalDue, LocalDate billedOn, Localdate dueBy, Boolean paidStatus, Integer accountantId) {
+        this.patientId = patientId;
+        this.totalDue = totalDue;
+        this.billedOn = billedOn;
+        this.dueBy = dueBy;
+        this.paidStatus = paidStatus;
+        this.accountantId = accountantId;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public Double getTotalDue() {
+        return totalDue;
+    }
+
+    public void setTotalDue(Double totalDue) {
+        this.totalDue = totalDue;
+    }
+
+    public LocalDate getBilledOn() {
+        return billedOn;
+    }
+
+    public void setBilledOn(LocalDate billedOn) {
+        this.billedOn = billedOn;
+    }
+
+    public Localdate getDueBy() {
+        return dueBy;
+    }
+
+    public void setDueBy(Localdate dueBy) {
+        this.dueBy = dueBy;
+    }
+
+    public Boolean getPaidStatus() {
+        return paidStatus;
+    }
+
+    public void setPaidStatus(Boolean paidStatus) {
+        this.paidStatus = paidStatus;
+    }
+
+    public Integer getAccountantId() {
+        return accountantId;
+    }
+
+    public void setAccountantId(Integer accountantId) {
+        this.accountantId = accountantId;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" + "patientId=" + patientId + ", totalDue=" + totalDue + ", billedOn=" + billedOn + ", dueBy=" + dueBy + ", paidStatus=" + paidStatus + ", accountantId=" + accountantId + '}';
+    }
+    
+    
+    
+    
     
 }
