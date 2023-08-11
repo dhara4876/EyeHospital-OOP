a8c04b8eac2611c091d56063dc8fd1dcbe642cb8
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-11 18:55:03 +0600

    user login method and accountant instance

--- src/Users/User.java
+++ src/Users/User.java
@@ -4,6 +4,11 @@
  */
 package Users;
 
+import Model.LoginInfo;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -75,6 +80,74 @@ public abstract class User implements Serializable{
         return "User{" + "name=" + name + ", ID=" + ID + ", password=" + password + ", email=" + email + ", gender=" + gender + ", DOB=" + DOB + '}';
     }
     
-
+    public static int userLogin(int idcheck, String passcheck){
+        File f = null;
+        FileInputStream fis = null;      
+        ObjectInputStream ois = null;
+        int idflag=0;
+        int passflag=0;
+        int userType=0;
+        try {
+            f = new File("LoginInfoObjects.bin");
+            fis = new FileInputStream(f);
+            ois = new ObjectInputStream(fis);
+            LoginInfo tempLogin;
+            try{
+                System.out.println("Printing login objects");
+                while(true){
+                    if (idflag==1){break;}
+                    tempLogin = (LoginInfo) ois.readObject();
+                    System.out.println(tempLogin.toString());
+                    if (idcheck==tempLogin.getUserId()){
+                        idflag=1;
+                        if (passcheck.equals(tempLogin.getUserPass())){
+                            passflag=1;
+                            if (tempLogin.getUserType().equals("Doctor")){userType=3;}
+                            else if (tempLogin.getUserType().equals("Patient")){userType=4;}
+                            else if (tempLogin.getUserType().equals("Pharmacist")){userType=5;}
+                            else if (tempLogin.getUserType().equals("Nurse")){userType=6;}
+                            else if (tempLogin.getUserType().equals("Director")){userType=7;}
+                            else if (tempLogin.getUserType().equals("Accountant")){userType=8;}
+                            else if (tempLogin.getUserType().equals("HROfficer")){userType=9;}
+                            else {userType=10;}
+                            break;
+                        }
+                    }
+                }
+            }
+            catch(IOException | ClassNotFoundException e){
+                System.out.println(e.toString());
+                System.out.println("IOException | ClassNotFoundException in reading bin file");
+            }
+            System.out.println("End of file\n");
+            
+            if (idflag==1){
+                if(passflag==1){
+                    //errorLabel.setText("Login Successful");
+                    return userType;
+                }
+                else{return 2;}
+                //code 2 - errorLabel.setText("Error, wrong password");
+            }
+            else{return 1;}
+                //code 1 - errorLabel.setText("Error, user not found");
+        
+        } catch (IOException ex) {
+            System.out.println("IOException on entire file handling");
+            return 0;
+        }
+        finally {
+            try {
+                if(ois != null) ois.close();
+            } catch (IOException ex) { }
+        }
+        //code 0 - unhandled exception
+    }
+       public static boolean isNumeric(String str) { 
+        try {  
+          Integer.parseInt(str);  
+          return true;
+        } catch(NumberFormatException e){return false;}
+    }
 }
 
