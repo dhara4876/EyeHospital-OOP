d0f0e59c3749f387fb7fffcf84418ad540567de1
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2023-08-25 00:32:33 +0600

    fixed the dashboard

---
+++ src/Nushrat/Pharmacist/PharmacistDashboardSceneController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Nushrat.Pharmacist;
+
+import Users.Pharmacist;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class PharmacistDashboardSceneController implements Initializable {
+    
+    private Pharmacist pharmacist; 
+
+    public Pharmacist getPharmacist() {
+        return pharmacist;
+    }
+
+    public void setPharmacist(Pharmacist pharmacist) {
+        this.pharmacist = pharmacist;
+    }
+    
+    
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
